263031e5b077ddd10e7fb220b30c5dd43ba86d34
JulianRios99 <julian-rios99@hotmail.com>
2023-03-28 17:07:29 -0400

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,58 +9,62 @@ const player2Name = urlParams.get("player2") || "Jugador 2";
 let currentPlayer = player1Name;
 
 gameBoard.innerHTML = Array.from({ length: rows }, () => Array(columns).fill(0))
-	.map(
-		(row, rowIndex) =>
-			row
-				.map(
-					(cell, colIndex) =>
-						`<div class="cell" data-row="${rowIndex}" data-col="${colIndex}"></div>`
-				)
-				.join('')
-	)
-	.join('');
+    .map(
+        (row, rowIndex) =>
+            row
+                .map(
+                    (cell, colIndex) =>
+                        `<div class="cell" data-row="${rowIndex}" data-col="${colIndex}"></div>`
+                )
+                .join('')
+    )
+    .join('');
 
 const cells = Array.from(document.querySelectorAll('.cell'));
 cells.forEach((cell) => {
-	cell.addEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick);
 });
 
-message.textContent = `${currentPlayer}, es tu turno`;
+message.textContent = `${currentPlayer}, es tu turnoo`;
 
 async function handleClick(event) {
-  const col = parseInt(event.target.dataset.col);
-
-  // Hacer una llamada API al backend para realizar un movimiento
-  const response = await fetch("/api/make-move?col=" + col, {
-    method: "POST",
-  });
-
-  const result = await response.json();
-
-  if (result.error) {
-    console.error(result.error);
-    return;
-  }
-
-  const { row, status, player } = result;
-
-  if (row !== null) {
-    await animateFallingPiece(row, col, player);
-
-    if (status === "win") {
-      message.textContent = `¡${currentPlayer} gana!`;
-      cells.forEach((cell) => cell.removeEventListener("click", handleClick));
-    } else {
-      currentPlayer = currentPlayer === player1Name ? player2Name : player1Name;
-      message.textContent = `${currentPlayer}, es tu turno`;
+    const col = parseInt(event.target.dataset.col);
+  
+    // Hacer una llamada API al backend para realizar un movimiento
+    const response = await fetch("/api/make-move?col=" + col, {
+      method: "POST",
+    });
+  
+    const result = await response.json();
+  
+    if (result.error) {
+      console.error(result.error);
+      return;
     }
-  }
-}
+  
+    const { row, status, player } = result;
+  
+    if (row !== null) {
+      const playerName = player === 1 ? player1Name : player2Name;
+      await animateFallingPiece(row, col, player, playerName);
+  
+      if (status === "win") {
+        message.textContent = `¡${playerName} gana!`;
+        cells.forEach((cell) => cell.removeEventListener("click", handleClick));
+      } else {
+        currentPlayer = playerName === player1Name ? player2Name : player1Name;
+        message.textContent = `${currentPlayer}, es tu turnoo`;
+      }
+    }
+  }  
+  
 
-async function animateFallingPiece(row, col, player) {
-  const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-  cell.classList.add(player === 1 ? "player1" : "player2");
-  cell.classList.add(player === 1 ? "player1-animate" : "player2-animate");
-  await new Promise((resolve) => setTimeout(resolve, 100));
-  cell.classList.remove(player === 1 ? "player1-animate" : "player2-animate");
-}
+
+  async function animateFallingPiece(row, col, player, playerName) {
+    const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+    cell.classList.add(playerName === player1Name ? "player1" : "player2");
+    cell.classList.add(playerName === player1Name ? "player1-animate" : "player2-animate");
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    cell.classList.remove(playerName === player1Name ? "player1-animate" : "player2-animate");
+  }
+  
